6cae2f4e31c2019edbb7e7c2eb13e5cb54dba4fc
Gokarna Adhikari <agokarna444@gmail.com>
2024-08-19 22:49:18 +0545

    change the image

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -42,7 +42,7 @@ const Home = () => {
                 <div className="md:w-1/3 w-full h-auto flex justify-center items-center">
                     <div className="relative w-full aspect-square rounded-full glow-border">
                         <div className="absolute inset-0 rounded-full border-[6px] border-transparent animate-rotate-border" />
-                        <img src={Profile} alt="profile img" className="w-full h-full rounded-full object-contain relative scale-110 bottom-3 right-2 z-10" />
+                        <img src="https://images.unsplash.com/photo-1687278698468-d529a2bda16b?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="profile img" className="w-full h-full rounded-full object-contain relative scale-110 bottom-3 right-2 z-10" />
                     </div>
                 </div>
             </div>
